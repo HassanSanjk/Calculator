f7435547fe364dd6163dde24a58262307cc2f5f3
Sanjk <elhassanmohammed123@gmail.com>
2024-12-15 20:34:38 +0800

    Added functionality

--- Calculator.py
+++ Calculator.py
@@ -6,7 +6,7 @@ class Calculator(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Calculator")
-        self.setGeometry(100, 100, 400, 500)
+        self.setGeometry(100, 100, 400, 300)
         self.label = QLabel("", self)
         self.button1 = QPushButton("1", self)
         self.button2 = QPushButton("2", self)
@@ -27,29 +27,44 @@ class Calculator(QMainWindow):
         self.buttonDot = QPushButton(".", self)
         self.buttonDEL = QPushButton("DEL",self)
 
-        """
-        self.button1.clicked.connect(self.button1Clicked)
-        self.button2.clicked.connect(self.button2Clicked)
-        self.button3.clicked.connect(self.button3Clicked)
-        self.button4.clicked.connect(self.button4Clicked)
-        self.button5.clicked.connect(self.button5Clicked)
-        self.button6.clicked.connect(self.button6Clicked)
-        self.button7.clicked.connect(self.button7Clicked)
-        self.button8.clicked.connect(self.button8Clicked)
-        self.button9.clicked.connect(self.button9Clicked)
-        self.button0.clicked.connect(self.button0Clicked)
+        buttons = [self.button1, self.button2, self.button3, self.button4, self.button5, self.button6, self.button7, self.button8, self.button9, self.button0, self.buttonMinus, self.buttonPlus, self.buttonMultiply, self.buttonDivide]
         
+        for button in buttons:
+            button.clicked.connect(self.buttonClicked)
 
-        self.buttonMinus.clicked.connect(self.buttonMinusClicked)
-        self.buttonPlus.clicked.connect(self.buttonPlusClicked)
-        self.buttonMultiply.clicked.connect(self.buttonMultiplyClicked)
-        self.buttonDivide.clicked.connect(self.buttonDivideClicked)
         self.buttonClear.clicked.connect(self.buttonClearClicked)
         self.buttonEqual.clicked.connect(self.buttonEqualClicked)
         self.buttonDot.clicked.connect(self.buttonDotClicked)
         self.buttonDEL.clicked.connect(self.buttonDELClicked)
-        """
+
         self.initUI()
+
+    def buttonClicked(self):
+        clicked_button = self.sender()
+        self.label.setText(self.label.text()+clicked_button.text())
+    
+    def buttonClearClicked(self):
+        self.label.setText("")
+
+    def buttonEqualClicked(self):
+        try:
+            result = eval(self.label.text())
+            self.label.setText(str(result))
+        except ZeroDivisionError:
+            self.label.setText("Cannot divide by zero")
+        except SyntaxError:
+            self.label.setText("Invalid syntax")
+        except Exception:
+            self.label.setText("Error!")
+
+    def buttonDotClicked(self):
+        if self.label.text() and self.label.text()[-1] != ".":
+            self.label.setText(self.label.text() + ".")
+
+    def buttonDELClicked(self):
+        if self.label.text():
+            self.label.setText(self.label.text()[:-1])
+
     def initUI(self):
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
@@ -58,7 +73,7 @@ class Calculator(QMainWindow):
         glayout.addWidget(self.button7, 0, 0)
         glayout.addWidget(self.button8, 0, 1)
         glayout.addWidget(self.button9, 0, 2)
-        glayout.addWidget(self.buttonDEL, 0, 3)
+        glayout.addWidget(self.buttonClear, 0, 3)
         glayout.addWidget(self.button4, 1, 0)
         glayout.addWidget(self.button5, 1, 1)
         glayout.addWidget(self.button6, 1, 2)
@@ -73,7 +88,7 @@ class Calculator(QMainWindow):
         glayout.addWidget(self.buttonMultiply, 3, 3)
 
         hbox = QHBoxLayout()
-        hbox.addWidget(self.buttonClear)
+        hbox.addWidget(self.buttonDEL)
         hbox.addWidget(self.buttonEqual)
 
         
@@ -85,7 +100,28 @@ class Calculator(QMainWindow):
         central_widget.setLayout(main_layout)
 
         self.setStyleSheet("""
-        
+        QPushButton {
+            font-size: 30px;
+            background-color: #f0f0f0;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        QPushButton:hover {
+            background-color: #e0e0e0;
+        }
+
+        QPushButton:pressed {
+            background-color: #d0d0d0;
+        }
+
+        QLabel {
+            font-size: 30px;
+            font-weight: bold;
+            background-color: #f0f0f0;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
         """)
 
 def main():
